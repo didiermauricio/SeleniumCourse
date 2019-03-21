c7ea867cc466de944e550d0d6545322636aa246a
didiermauricio <didiermauricio@gmail.com>
2019-03-21 22:53:54 +0100

    New Files for learn

---
+++ SeleniumWD2Tutorial/src/ChromeDriverWindows.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeDriverWindows {
+
+	public static void main (String args []) {
+		
+		String baseUrl = "http://www.google.com";
+		WebDriver driver;
+		
+		System.setProperty("webdriver.gecko.driver", "E:\\01_UDEMY\\SeleniumCourse\\Libraries\\Gheko\\geckodriver.exe");
+		driver = new ChromeDriver();
+		driver.get(baseUrl);
+	}
+}
---
+++ SeleniumWD2Tutorial/src/FireFoxDemo.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FireFoxDemo {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver;
+		System.setProperty("webdriver.gecko.driver", "E:\\01_UDEMY\\SeleniumCourse\\Libraries\\Gheko\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		String baseUrl = "http://www.google.com";
+		
+		driver.get(baseUrl);
+		
+		
+
+	}
+
+}
